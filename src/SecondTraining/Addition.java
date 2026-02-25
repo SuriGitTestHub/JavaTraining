e760981b9b4513595d5d46c2c6d9cf89b48effd3
TestCodHub <kassutracker@gmail.com>
2026-02-26 02:51:12 +0530

    Java test

---
+++ src/SecondTraining/Addition.java
@@ -0,0 +1,19 @@
+package SecondTraining;
+import java.util.*;
+
+public class Addition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of A & B");
+        int a = sc.nextInt();
+        // int a=Integer.parseInt(sc.nextLine().trim());
+      //  System.out.println("Enter the value of B");
+        //int b=Integer.parseInt(sc.nextLine().trim());
+        int b= sc.nextInt();
+        int c= a+b;
+        System.out.println(c);
+
+
+
+    }
+}
